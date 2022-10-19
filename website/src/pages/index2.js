9d96376e32f4897ccf5d475dc1467449cd60645d
Merill Fernando <merill@merill.net>
2022-10-19 15:52:05 +1100

    Fixed build issue

--- website/src/pages/index2.js
+++ website/src/pages/index2.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs">
+            to="/">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
