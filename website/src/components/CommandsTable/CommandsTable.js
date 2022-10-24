510f22e1b7b255286ca032a9ed3067e9ca1c72fd
Merill Fernando <merill@merill.net>
2022-10-24 12:49:01 +1100

    Fixed scrollbar issue

--- website/src/components/CommandsTable/CommandsTable.js
+++ website/src/components/CommandsTable/CommandsTable.js
@@ -73,10 +73,8 @@ const CommandsDataTable = ({
         />
       </div>
 
-      <div className="mt-2 flex flex-col">
-        <div className="-my-2 overflow-x-auto -mx-4 sm:-mx-6 lg:-mx-8">
-          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div className="overflow-hidden">
+          <div className="py-2 ">
+            <div className="overflow-x-auto">
               <table
                 {...getTableProps()}
                 className="min-w-full divide-y divide-gray-200 table-fixed"
@@ -142,9 +140,7 @@ const CommandsDataTable = ({
                 </tbody>
               </table>
             </div>
-          </div>
         </div>
-      </div>
     </>
   );
 };
